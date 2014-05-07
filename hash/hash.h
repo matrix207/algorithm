cde3de5658c934e3aa140f056a21cacce4a8f693
dennis_dell <dennis.cpp@gmail.com>
2014-05-07 12:12:28 +0800

    update

--- hash/hash.h
+++ hash/hash.h
@@ -4,27 +4,27 @@
 /* debug macro */
 #define GO_HERE() printf("%s %s %d\n", __FILE__, __func__, __LINE__)
 
-struct entry_s {
+struct node {
 	char *key;
 	char *value;
-	struct entry_s *next;
+	struct node *next;
 };
 
-struct hashtable_s {
+struct hashtable {
 	unsigned long int size;
-	struct entry_s **table;	
+	struct node **table;	
 };
 
-struct hashtable_s *ht_create( unsigned long size );
+struct hashtable *ht_create( unsigned long size );
 
-void ht_set( struct hashtable_s *hashtable, char *key, char *value );
+void ht_set( struct hashtable *pht, char *key, char *value );
 
-char *ht_get( struct hashtable_s *hashtable, char *key );
+char *ht_get( struct hashtable *pht, char *key );
 
-int ht_del( struct hashtable_s *hashtable, char *key );
+int ht_del( struct hashtable *pht, char *key );
 
-void ht_release( struct hashtable_s *hashtable );
+void ht_release( struct hashtable *pht );
 
-void show_hash_table(struct hashtable_s *hashtable);
+void show_hash_table(struct hashtable *pht);
 
 #endif /* HASH_H */
